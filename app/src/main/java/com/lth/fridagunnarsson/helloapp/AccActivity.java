53191ff0eb8c7d86c3b9e9ae38bc05379f315712
Frida Gunnarsson <fridagunnarsson.94@gmail.com>
2018-04-12 14:30:37 +0200

    Finished

--- app/src/main/java/com/lth/fridagunnarsson/helloapp/AccActivity.java
+++ app/src/main/java/com/lth/fridagunnarsson/helloapp/AccActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 import static android.util.Half.EPSILON;
 
 
@@ -35,10 +37,10 @@ public class AccActivity extends AppCompatActivity implements SensorEventListene
         setContentView(R.layout.activity_acc);
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        acc_text_x = (TextView) findViewById(R.id.txt_X);
-        acc_text_y = (TextView) findViewById(R.id.txt_Y);
-        acc_text_z = (TextView) findViewById(R.id.txt_Z);
-        start();
+        acc_text_x = findViewById(R.id.txt_X);
+        acc_text_y = findViewById(R.id.txt_Y);
+        acc_text_z = findViewById(R.id.txt_Z);
+        //start();
     }
 
 
@@ -59,108 +61,22 @@ public class AccActivity extends AppCompatActivity implements SensorEventListene
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-// This time step's delta rotation to be multiplied by the current rotation
-        // after computing it from the gyro sample data.
-        /*
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-          deltaRotationVector = lowPass(event.values.clone(), deltaRotationVector);
-       }*/
 
-        float axisX = event.values[0];
-        float axisY = event.values[1];
-        float axisZ = event.values[2];
+        //float axisX = event.values[0];
+        //float axisY = event.values[1];
+        //float axisZ = event.values[2];
 
-        /*
-        if (timestamp != 0) {
-            final float dT = (event.timestamp - timestamp) * NS2S;
-            // Axis of the rotation sample, not normalized yet.
-            axisX = event.values[0];
-            axisY = event.values[1];
-            axisZ = event.values[2];
-
-            // Calculate the angular speed of the sample
-            float omegaMagnitude = (float) Math.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
+        DecimalFormat df = new DecimalFormat("0.0000");
 
 
-            // Normalize the rotation vector if it's big enough to get the axis
-            if (omegaMagnitude > EPSILON) {
-                axisX /= omegaMagnitude;
-                axisY /= omegaMagnitude;
-                axisZ /= omegaMagnitude;
-            }
-
-            // Integrate around this axis with the angular speed by the time step
-            // in order to get a delta rotation from this sample over the time step
-            // We will convert this axis-angle representation of the delta rotation
-            // into a quaternion before turning it into the rotation matrix.
-            float thetaOverTwo = omegaMagnitude * dT / 2.0f;
-            float sinThetaOverTwo = (float) Math.sin(thetaOverTwo);
-            float cosThetaOverTwo = (float) Math.cos(thetaOverTwo);
-            deltaRotationVector[0] = sinThetaOverTwo * axisX;
-            deltaRotationVector[1] = sinThetaOverTwo * axisY;
-            deltaRotationVector[2] = sinThetaOverTwo * axisZ;
-            deltaRotationVector[3] = cosThetaOverTwo;
-        }
-        timestamp = event.timestamp;
-        float[] deltaRotationMatrix = new float[9];
-        SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
-        // User code should concatenate the delta rotation we computed with the current
-        // rotation in order to get the updated rotation.
-        // rotationCurrent = rotationCurrent * deltaRotationMatrix;*/
-
-        acc_text_x.setText("X: " + axisX);
-        acc_text_y.setText("Y: " + axisY);
-        acc_text_z.setText("Z: " + axisZ);
+        acc_text_x.setText("X: " + df.format(event.values[0]));
+        acc_text_y.setText("Y: " + df.format(event.values[1]));
+        acc_text_z.setText("Z: " + df.format(event.values[2]));
     }
 
-    public void stop() {
-        if(haveSensor && haveSensor2){
-            mSensorManager.unregisterListener(this,mAccelerometer);
-            mSensorManager.unregisterListener(this,mMagnetometer);
-        }
-        else{
-            if(haveSensor)
-                mSensorManager.unregisterListener(this,mRotationV);
-        }
-    }
 
-    public void start() {
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) == null) {
-            if ((mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) == null) || (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) == null)) {
-                noSensorsAlert();
-            }
-            else {
-                mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-                mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-                haveSensor = mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
-                haveSensor2 = mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_UI);
-            }
-        }
-        else{
-            mRotationV = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-            haveSensor = mSensorManager.registerListener(this, mRotationV, SensorManager.SENSOR_DELAY_UI);
-        }
-    }
 
-    public void noSensorsAlert(){
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setMessage("Your device doesn't support the Compass.")
-                .setCancelable(false)
-                .setNegativeButton("Close",new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        finish();
-                    }
-                });
-        alertDialog.show();
-    }
 
-    protected float[] lowPass( float[] input, float[] output ) {
-        if ( output == null ) return input;
-        for ( int i=0; i<input.length; i++ ) {
-            output[i] = output[i] + ALPHA * (input[i] - output[i]);
-        }
-        return output;
-    }
 
 }
 
--- app/src/main/java/com/lth/fridagunnarsson/helloapp/Hello.java
+++ app/src/main/java/com/lth/fridagunnarsson/helloapp/Hello.java
@@ -4,9 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
-import android.widget.EditText;
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class Hello extends AppCompatActivity {
 
@@ -19,17 +16,11 @@ public class Hello extends AppCompatActivity {
     public void sendToAcc(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, AccActivity.class);
-        //EditText editText1 = (EditText) findViewById(R.id.editText1);
-        //String message = editText1.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 
     public void sendToCompass(View view) {
         Intent intent = new Intent(this, CompassActivity.class);
-        //EditText editText2 = (EditText) findViewById(R.id.editText2);
-        //String message = editText2.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
 
     }
